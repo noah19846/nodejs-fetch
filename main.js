72779d02926951534050daaaeedf78ec42c2b37b
yelqgd <yelqgd@gmail.com>
2021-05-27 16:22:15 +0800

    add Response, Request, fetch

--- main.js
+++ main.js
@@ -1,3 +1,68 @@
-const { Headers } = require('./lib')
+const http = require('http')
 
-console.log(Headers)
+const { Headers, Request, Response } = require('./lib')
+
+function fetch(input, init) {
+  return new Promise((resolve, reject) => {
+    const request = new Request(input, init)
+    const { headers = {} } = request
+
+    http
+      .request(
+        request.url,
+        {
+          method: request.method,
+          headers
+        },
+        res => {
+          let error
+          const { statusCode, statusMessage } = res
+
+          if (statusCode !== 200) {
+            error = new Error(
+              'Request Failed.\n' + `Status Code: ${statusCode}`
+            )
+          }
+
+          if (error) {
+            reject(error)
+            res.resume()
+
+            return
+          }
+
+          let rawData = Buffer.alloc(0)
+
+          res.on('data', chunk => {
+            rawData = Buffer.concat([rawData, chunk])
+          })
+
+          res.on('end', () => {
+            try {
+              resolve(
+                new Response(rawData, {
+                  status: statusCode,
+                  statusText: statusMessage,
+                  headers: res.headers,
+                  url: request.url
+                })
+              )
+            } catch (e) {
+              reject(e)
+            }
+          })
+        }
+      )
+      .on('error', e => {
+        reject(e)
+      })
+      .end()
+  })
+}
+
+module.exports = {
+  Headers,
+  Request,
+  Response,
+  fetch
+}
